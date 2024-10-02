f8cf3effaf82c6a657c970856b2293dbd52aed2c
Hemanth <mrhemanth@icloud.com>
2024-10-02 21:12:24 +0530

    Navigation updates.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
         <div className="flex gap-4 items-center flex-col sm:flex-row ">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-40"
-            href="http://localhost:3000/signup"
+            href="/signup"
             target="_self"
             rel="noopener noreferrer"
           >
@@ -17,7 +17,7 @@ export default function Home() {
           </a>
           <a
             className="rounded-full border border-solid border-black/[.99] dark:border-white/[.99] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-40"
-            href="http://localhost:3000/login"
+            href="/login"
             target="_self"
             rel="noopener noreferrer"
           >
