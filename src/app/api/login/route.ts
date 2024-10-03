d16fc350edce02c0da29d6b880236c7c936dd75f
Hemanth <mrhemanth@icloud.com>
2024-10-03 10:37:53 +0530

    Improved email login alert function.

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -1,18 +1,38 @@
 import { NextRequest, NextResponse } from "next/server";
 import { queryDatabase } from "@/app/service/database";
 import { cookies } from "next/headers";
+import IPinfoWrapper, { IPinfo, AsnResponse } from "node-ipinfo";
 
 import { EmailClient } from "@azure/communication-email";
 import jwt from 'jsonwebtoken';
 
-
-
-async function sendLoginEmail( email: string, ip: string, city: string, country: string) {
+async function sendTestLoginEamil(email: string, ip: string) {
   const connectionString = process.env.AZURE_COMMUNICATION_EMAIL_CONNECTION_STRING;
   if (!connectionString) {
     throw new Error("Azure Communication Email connection string is not defined.");
   }
   const client = new EmailClient(connectionString);
+  let body = "Your account was logged into from the IP address " + ip + ". Go to https://ipinfo.io/" + ip + " to see more details of the IP address.";
+  try {
+    const ipinfoToken = process.env.IPINFO_TOKEN;
+    if (!ipinfoToken) {
+      throw new Error("IPinfo token is not defined.");
+    }
+    const ipinfoWrapper = new IPinfoWrapper(ipinfoToken);
+    const ipinfo = await ipinfoWrapper.lookupIp(ip);
+    console.log("IP info: ", ipinfo);
+    const city = ipinfo.city;
+    const country = ipinfo.country;
+    const org = ipinfo.org;
+    const loc = ipinfo.loc;
+    console.log("City: ", city);
+    console.log("Country: ", country);
+    body = `Your account was logged in from the IP 
+    address ${ip} in ${city}, ${country}. ISP is ${org}. Coordinates ${loc}`;
+
+  } catch (e) {
+    console.error(e);
+  }
   const from = "DoNotReply@sample.hemanth.systems";
     const to = email;
     console.log("Sending email to: ", to);
@@ -20,7 +40,7 @@ async function sendLoginEmail( email: string, ip: string, city: string, country:
         senderAddress: from,
         content: {
             subject: "Login Alert",
-            plainText: `Your account was logged into from the IP address ${ip} in ${city}, ${country}. Go to https://ipinfo.io/${ip} to see more details of hte IP address.`,
+            plainText: body,
         },
         recipients: {
             to: [{address: to}],
@@ -31,11 +51,34 @@ async function sendLoginEmail( email: string, ip: string, city: string, country:
     console.log("Email result: ", finalize);
 }
 
+// async function sendLoginEmail( email: string, ip: string, city: string, country: string) {
+//   const connectionString = process.env.AZURE_COMMUNICATION_EMAIL_CONNECTION_STRING;
+//   if (!connectionString) {
+//     throw new Error("Azure Communication Email connection string is not defined.");
+//   }
+//   const client = new EmailClient(connectionString);
+//   const from = "DoNotReply@sample.hemanth.systems";
+//     const to = email;
+//     console.log("Sending email to: ", to);
+//     const message = {
+//         senderAddress: from,
+//         content: {
+//             subject: "Login Alert",
+//             plainText: `Your account was logged into from the IP address ${ip} in ${city}, ${country}. Go to https://ipinfo.io/${ip} to see more details of hte IP address.`,
+//         },
+//         recipients: {
+//             to: [{address: to}],
+//         }
+//     };
+//     const poller = await client.beginSend(message);
+//     const finalize = await poller.pollUntilDone();
+//     console.log("Email result: ", finalize);
+// }
+
 export async function POST(request: NextRequest) {
   const { email, password } = await request.json();
+  // using dummy ip for testing to see the working of ipinfo api for prod use vercel headers to et the ip 
   const ip = (request.headers.get('x-forwarded-for') ?? '152.59.194.253').split(',')[0];
-  const city = (request.headers.get('x-vercel-ip-city') ?? 'Vijayawada');
-  const country = (request.headers.get('x-vercel-ip-country') ?? 'India');
   console.log(ip)
   try { 
     const userResult = await queryDatabase(
@@ -47,7 +90,7 @@ export async function POST(request: NextRequest) {
   console.log("USER", user[0].email);
   console.log("USER", user[0].password);
   if (user[0].email === email && user[0].password === password) {
-    sendLoginEmail(email, ip, city, country);
+    sendTestLoginEamil(email, ip)
     const jwtSecret = process.env.JWT_SECRET;
     if (!jwtSecret) {
       throw new Error("JWT secret is not defined.");
