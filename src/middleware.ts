5bd34d0010bc26e2468087b3d14850deb5eb2e04
Hemanth <mrhemanth@icloud.com>
2024-10-07 21:43:27 +0530

    Redirect issues.

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,22 +1,22 @@
-// import { NextResponse } from 'next/server'
-// import type { NextRequest } from 'next/server'
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
 export { default } from "next-auth/middleware"
 
-// export async function middleware(request: NextRequest) {
-//   const token = request.cookies.get('session')?.value
+export async function middleware(request: NextRequest) {
+  const token = request.cookies.get('session')?.value
     
-//   if (request.nextUrl.pathname === '/login' || request.nextUrl.pathname === '/signup') {
-//     if (token) {
-//         return NextResponse.redirect(new URL('/pay', request.url))
-//     }
-//   } else if (request.nextUrl.pathname === '/pay') {
-//     if (!token) {
-//       return NextResponse.redirect(new URL('/login', request.url))
-//     }
-//   } 
-//   return NextResponse.next()
-// }
+  if (request.nextUrl.pathname === '/login' || request.nextUrl.pathname === '/signup') {
+    if (token) {
+        return NextResponse.redirect(new URL('/pay', request.url))
+    }
+  } else if (request.nextUrl.pathname === '/pay') {
+    if (!token) {
+      return NextResponse.redirect(new URL('/login', request.url))
+    }
+  } 
+  return NextResponse.next()
+}
 
 export const config = {
-  matcher: ['/login', '/pay1', '/signup', '/pay'],
+  matcher: ['/login', '/signup', '/pay'],
 }
